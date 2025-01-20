46addb6c288299f36c979b950d4c2640d9880954
erinogamer <windows12videos@gmail.com>
2025-01-20 14:46:37 +0100

    project

--- main.ts
+++ main.ts
@@ -1,7 +1,63 @@
-basic.forever(function on_forever() {
-    
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        . . . . #
+        `)
+    basic.showLeds(`
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        # . . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        # # . # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        # # # # .
+        . . . # .
+        . . . # .
+        . . . # .
+        `)
+    basic.showString("EXPLOTANO TOWER")
 })
-function on_button_pressed_a() {
-    basic.showArrow(ArrowNames.South)
-}
-
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(randint(87916579, 99999999))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("ok ok")
+})
+input.onGesture(Gesture.FreeFall, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showString("DAJ FOLLOW")
+})
