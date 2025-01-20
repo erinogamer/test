6cb51203c956cdf480e274766adafa4b23e7589c
erinogamer <windows12videos@gmail.com>
2025-01-20 14:23:54 +0100

    just a new school project

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
-
 def on_button_pressed_a():
-    basic.show_arrow(ArrowNames.SOUTH)
-
+    basic.show_string("Hello!")
+input.on_button_pressed(Button.A, on_button_pressed_a)
